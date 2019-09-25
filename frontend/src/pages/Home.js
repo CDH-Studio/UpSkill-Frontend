d6c89f595b57fc0a26381e4fba735f3ccb058701
Rizvi <rizvida1@gmail.com>
2019-09-25 15:50:06 -0400

    Added background and dropdown

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,26 @@
 import React, { Component } from "react";
-import { Button, Icon } from "semantic-ui-react";
+import { Button, Icon, Dropdown } from "semantic-ui-react";
 
+const options = [
+  { key: "angular", text: "Angular", value: "angular" },
+  { key: "css", text: "CSS", value: "css" },
+  { key: "design", text: "Graphic Design", value: "design" },
+  { key: "ember", text: "Ember", value: "ember" },
+  { key: "html", text: "HTML", value: "html" },
+  { key: "ia", text: "Information Architecture", value: "ia" },
+  { key: "javascript", text: "Javascript", value: "javascript" },
+  { key: "mech", text: "Mechanical Engineering", value: "mech" },
+  { key: "meteor", text: "Meteor", value: "meteor" },
+  { key: "node", text: "NodeJS", value: "node" },
+  { key: "plumbing", text: "Plumbing", value: "plumbing" },
+  { key: "python", text: "Python", value: "python" },
+  { key: "rails", text: "Rails", value: "rails" },
+  { key: "react", text: "React", value: "react" },
+  { key: "repair", text: "Kitchen Repair", value: "repair" },
+  { key: "ruby", text: "Ruby", value: "ruby" },
+  { key: "ui", text: "UI Design", value: "ui" },
+  { key: "ux", text: "User Experience", value: "ux" }
+];
 export default class Home extends Component {
   goto = link => this.props.history.push(link);
 
@@ -8,6 +28,14 @@ export default class Home extends Component {
     return (
       <div>
         <h2>Home</h2>
+        <Dropdown
+          placeholder="Skillzzz"
+          fluid
+          multiple
+          search
+          selection
+          options={options}
+        />
         <Button
           color="orange"
           inverted
