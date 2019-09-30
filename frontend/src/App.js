af8dfbe75ef9bfb25a099515fd60833fb248831f
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2019-09-30 13:54:40 -0400

    alphabetised

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Home, About, Landing, Advanced } from "./pages/index";
+import { About, Advanced, Home, Landing } from "./pages/index";
 
 class App extends Component {
   goto = link => this.props.history.push(link);
