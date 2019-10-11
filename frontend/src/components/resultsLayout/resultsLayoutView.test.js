1a67843c63809a777ea5f4f6262aa4c1e9ea6fb9
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2019-10-11 11:28:14 -0400

    fixed resultsLayout test

--- frontend/src/components/resultsLayout/resultsLayoutView.test.js
+++ frontend/src/components/resultsLayout/resultsLayoutView.test.js
@@ -6,12 +6,8 @@ import ResultsLayoutView from "./resultsLayoutView";
 it("home layout view contains critical components", () => {
   const wrapper = shallow(<ResultsLayoutView />);
 
-  //search button not added yet
-  //const searchButtons = wrapper.find("#searchButton");
-  //expect(searchButtons.length).toBe(1);
-
-  const searchForms = wrapper.find("injectIntl(SearchFormController)");
-  expect(searchForms.length).toBe(1);
+  const searchButtons = wrapper.find(".resultContent");
+  expect(searchButtons.length).toBe(1);
 
   const navigationBars = wrapper.find("NavigationBarController");
   expect(navigationBars.length).toBe(1);
