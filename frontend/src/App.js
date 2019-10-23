432d329e3c991447cf5c7075f0ca25a0394edd4d
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2019-10-23 10:17:56 -0400

    removed temp keycloak removal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,7 @@ import { IntlProvider } from "react-intl";
 
 import messages_en from "./i18n/en_CA.json";
 import messages_fr from "./i18n/fr_CA.json";
-
-//import "./styles.global.less";
 import "./App.css";
-
 import { About, Advanced, Home, Landing, Results } from "./pages/index";
 
 let localLang = (() => {
@@ -52,14 +49,14 @@ class App extends Component {
     this.changeLanguage = this.changeLanguage.bind(this);
   }
 
-  /*componentDidMount() {
+  componentDidMount() {
     const keycloak = Keycloak("/keycloak.json");
     keycloak
       .init({ onLoad: "login-required", promiseType: "native" })
       .then(authenticated => {
         this.setState({ keycloak: keycloak, authenticated: authenticated });
       });
-  }*/
+  }
 
   goto = link => history.push(link);
 
@@ -70,7 +67,7 @@ class App extends Component {
         "background: linear-gradient(22.5deg, rgba(67, 67, 67, 0.02) 0%, rgba(67, 67, 67, 0.02) 29%,rgba(47, 47, 47, 0.02) 29%, rgba(47, 47, 47, 0.02) 37%,rgba(23, 23, 23, 0.02) 37%, rgba(23, 23, 23, 0.02) 55%,rgba(182, 182, 182, 0.02) 55%, rgba(182, 182, 182, 0.02) 69%,rgba(27, 27, 27, 0.02) 69%, rgba(27, 27, 27, 0.02) 71%,rgba(250, 250, 250, 0.02) 71%, rgba(250, 250, 250, 0.02) 100%),linear-gradient(67.5deg, rgba(117, 117, 117, 0.02) 0%, rgba(117, 117, 117, 0.02) 14%,rgba(199, 199, 199, 0.02) 14%, rgba(199, 199, 199, 0.02) 40%,rgba(33, 33, 33, 0.02) 40%, rgba(33, 33, 33, 0.02) 48%,rgba(135, 135, 135, 0.02) 48%, rgba(135, 135, 135, 0.02) 60%,rgba(148, 148, 148, 0.02) 60%, rgba(148, 148, 148, 0.02) 95%,rgba(53, 53, 53, 0.02) 95%, rgba(53, 53, 53, 0.02) 100%),linear-gradient(135deg, rgba(190, 190, 190, 0.02) 0%, rgba(190, 190, 190, 0.02) 6%,rgba(251, 251, 251, 0.02) 6%, rgba(251, 251, 251, 0.02) 18%,rgba(2, 2, 2, 0.02) 18%, rgba(2, 2, 2, 0.02) 27%,rgba(253, 253, 253, 0.02) 27%, rgba(253, 253, 253, 0.02) 49%,rgba(128, 128, 128, 0.02) 49%, rgba(128, 128, 128, 0.02) 76%,rgba(150, 150, 150, 0.02) 76%, rgba(150, 150, 150, 0.02) 100%),linear-gradient(90deg, #FFF,#FFF);";
     }
 
-    const keycloak = true;
+    const keycloak = this.state.keycloak;
     if (keycloak) {
       console.log(
         "Render: ",
@@ -79,7 +76,7 @@ class App extends Component {
         i18nConfig.locale
       );
 
-      if (true)
+      if (this.state.authenticated)
         return (
           <IntlProvider
             locale={i18nConfig.locale}
