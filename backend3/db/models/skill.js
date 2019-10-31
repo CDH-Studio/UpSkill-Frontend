fdc0c916de4c6633895d64d09b02069cde84b97c
Rizvi <rizvida1@gmail.com>
2019-10-31 15:18:13 -0400

    Added setup migration

--- backend3/db/models/skill.js
+++ backend3/db/models/skill.js
@@ -1,11 +1,15 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const skill = sequelize.define('skill', {
-    description_en: DataTypes.STRING,
-    description_fr: DataTypes.STRING
-  }, {});
+  const skill = sequelize.define(
+    "skill",
+    {
+      description_en: DataTypes.STRING,
+      description_fr: DataTypes.STRING
+    },
+    {}
+  );
   skill.associate = function(models) {
-    // associations can be defined here
+    skill.belongsToMany(models.user, { through: "user_skills" });
   };
   return skill;
 };
--- backend3/db/models/user.js
+++ backend3/db/models/user.js
@@ -1,23 +1,27 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const user = sequelize.define('user', {
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    job_title: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    mobile: DataTypes.STRING,
-    location: DataTypes.STRING,
-    manager: DataTypes.STRING,
-    team: DataTypes.STRING,
-    first_language: DataTypes.STRING,
-    second_language: DataTypes.STRING,
-    year_service: DataTypes.INTEGER,
-    linkedin: DataTypes.STRING,
-    github: DataTypes.STRING
-  }, {});
+  const user = sequelize.define(
+    "user",
+    {
+      first_name: DataTypes.STRING,
+      last_name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      job_title: DataTypes.STRING,
+      phone: DataTypes.STRING,
+      mobile: DataTypes.STRING,
+      location: DataTypes.STRING,
+      manager: DataTypes.STRING,
+      team: DataTypes.STRING,
+      first_language: DataTypes.STRING,
+      second_language: DataTypes.STRING,
+      year_service: DataTypes.INTEGER,
+      linkedin: DataTypes.STRING,
+      github: DataTypes.STRING
+    },
+    {}
+  );
   user.associate = function(models) {
-    // associations can be defined here
+    user.belongsToMany(models.skill, { through: "user_skills" });
   };
   return user;
 };
