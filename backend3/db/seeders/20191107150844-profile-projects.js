46cee0f7108f7ddf87a9ebd9535fb3b53688f342
Mamadou Bah <bmama067@uottawa.ca>
2019-11-07 10:26:12 -0500

    Added default projects to profiles

--- backend3/db/seeders/20191107150844-profile-projects.js
+++ backend3/db/seeders/20191107150844-profile-projects.js
@@ -1,17 +1,41 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
     */
+    return queryInterface.bulkInsert(
+      "profileProjects",
+      [
+        {
+          id: "148b12da-0172-11ea-8d71-362b9e155667",
+          description:
+            "This project will develop a module offered to level 2 Undergraduate students and will seek to develop student’s skills in collaborative working and information literacy while still advancing their discipline knowledge.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+        },
+        {
+          id: "1d5c73b8-0172-11ea-8d71-362b9e155667",
+          description:
+            "This project will  focus on collaborative inquiry. Groups will negotiate a topic to investigate with the module leader",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+        },
+        {
+          id: "bce579b6-0172-11ea-8d71-362b9e155667",
+          description:
+            "This project will  focus on collaborative inquiry. Groups will negotiate a topic to investigate with the module leader",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          profileId: "6becd47a-ffe5-11e9-8d71-362b9e155667"
+        }
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
@@ -22,5 +46,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
+    return queryInterface.bulkDelete("profileProjects", null, {});
   }
 };
