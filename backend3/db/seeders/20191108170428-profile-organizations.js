e85ae5cb5f82cc227ee58bcdcdd7b2c3bbbdf75a
Mamadou Bah <bmama067@uottawa.ca>
2019-11-08 12:20:34 -0500

    Update 20191108170428-profile-organizations.js

--- backend3/db/seeders/20191108170428-profile-organizations.js
+++ backend3/db/seeders/20191108170428-profile-organizations.js
@@ -10,16 +10,74 @@ module.exports = {
       "profileOrganizations",
       [
         {
+          organization: "Innovation, Science and Economic Development Canada",
+          tier: "0",
           createdAt: new Date(),
           updatedAt: new Date(),
-          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667",
-          competencyId: "3a57095e-0174-11ea-8d71-362b9e155667"
+          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
         },
         {
+          organization: "DIGITAL TRANSFORMATION SERVICE SECTOR",
+          tier: "1",
           createdAt: new Date(),
           updatedAt: new Date(),
-          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667",
-          competencyId: "4beafb9e-0174-11ea-8d71-362b9e155667"
+          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+        },
+        {
+          organization: "Chief Information Office",
+          tier: "2",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+        },
+        {
+          organization: "Digital Services Division",
+          tier: "3",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+        },
+        {
+          organization: "Business Line Solutions Directorate",
+          tier: "4",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+        },
+        {
+          organization: "Innovation, Science and Economic Development Canada",
+          tier: "0",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+        },
+        {
+          organization: "DIGITAL TRANSFORMATION SERVICE SECTOR",
+          tier: "1",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+        },
+        {
+          organization: "Chief Information Office",
+          tier: "2",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+        },
+        {
+          organization: "Digital Services Division",
+          tier: "3",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
+        },
+        {
+          organization: "Director General's Office",
+          tier: "4",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
         }
       ],
       {}
