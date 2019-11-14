570ff076e3f46e5c6a936654135a0f7807fddfbc
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2019-11-14 17:01:36 -0500

    working on dropdowns

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { injectIntl } from "react-intl";
 import axios from "axios";
-
+import config from "../config";
 import ProfileLayoutController from "../components/profileLayout/profileLayoutController";
+
+const backendAddress = config.backendAddress;
 class Profile extends Component {
   goto = link => this.props.history.push(link);
 
@@ -13,14 +15,16 @@ class Profile extends Component {
     const handleSuccess = response => {
       console.log("Recieved", response);
 
-      this.setState({ profileInfo: response.data });
+      let profileInfo = response.data;
+
+      this.setState({ profileInfo: profileInfo });
     };
 
     axios
       .get(
         // "http://localhost:8080/api/profile/6becd47a-ffe5-11e9-8d71-362b9e155667"
         // "http://localhost:8080/api/profile/faba08aa-ffe3-11e9-8d71-362b9e155667"
-        "http://localhost:8080/api/profile/" + localStorage.getItem("userId")
+        backendAddress + "api/profile/" + localStorage.getItem("userId")
       )
       .then(handleSuccess)
       .catch(function(error) {
