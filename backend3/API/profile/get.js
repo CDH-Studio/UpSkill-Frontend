ba610e06086d9cbdb9a8a0feacd5114baf6c51c6
Mamadou Bah <bmama067@uottawa.ca>
2019-11-14 16:49:56 -0500

    Fixing Urls

--- backend3/API/profile/get.js
+++ backend3/API/profile/get.js
@@ -118,7 +118,7 @@ const getProfileById = async (request, response) => {
     email: data.email,
     firstLanguage: "English",
     firstName: data.firstName,
-    githubUrl: "https://www.google.com",
+    githubUrl: data.github,
     gradedOnSecondLanguage: true,
     classification: groupLevel ? groupLevel.description : null,
     jobTitle: data.jobTitle,
@@ -160,12 +160,11 @@ const getProfileById = async (request, response) => {
     talentMatrixResult: "Exceptional talent",
     team: data.team,
     telephone: data.telephone,
-    twitterUrl: "https://twitter.com/?lang=en",
+    twitterUrl: data.twitter,
     yearsOfService: data.yearService
   };
 
   response.status(200).json(resData);
-  // response.status(200).send("In Development");
 };
 
 module.exports = {
