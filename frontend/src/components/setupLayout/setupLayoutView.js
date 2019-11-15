20eba0083179c15fc26a0f30b21b5b8c53bc8b42
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2019-11-15 13:13:30 -0500

    began workign on getting geds info for profile creation

--- frontend/src/components/setupLayout/setupLayoutView.js
+++ frontend/src/components/setupLayout/setupLayoutView.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
-import { Grid, Button, Card, Dimmer, Loader } from "semantic-ui-react";
+import {
+  Grid,
+  Button,
+  Card,
+  Dimmer,
+  Loader,
+  Modal,
+  CardHeader
+} from "semantic-ui-react";
 
 export default class RegisterLayoutView extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     const {
       editProfileOptions,
@@ -10,7 +21,10 @@ export default class RegisterLayoutView extends Component {
       setFormChanges,
       setFormIndex,
       profileInfo,
-      formIndex
+      gedsInfoList,
+      setGedsIndex,
+      formIndex,
+      gedsIndex
     } = this.props;
 
     if (editProfileOptions === null) {
@@ -21,41 +35,67 @@ export default class RegisterLayoutView extends Component {
       );
     }
 
-    return (
-      <Grid style={{ maxWidth: "1250px", margin: "0px auto" }}>
-        <Grid.Column width={4}>{this.renderButtons()}</Grid.Column>
-        <Grid.Column width={12}>
-          <Card fluid>
-            <Card.Header>{formList[formIndex].name}</Card.Header>
-            <Card.Content>
-              {React.createElement(formList[formIndex].form, {
-                handleRegister: handleRegister,
-                setFormChanges: setFormChanges,
-                editProfileOptions: editProfileOptions,
+    if (!gedsInfoList) {
+      let err = gedsInfoList["fuck"]["my"]["shit"]["up"];
+    }
 
-                profileInfo: profileInfo,
-                handleNext: fieldsUpdate => {
-                  formList[formIndex].changes = {
-                    ...formList[formIndex].changes,
-                    ...fieldsUpdate
-                  };
-                  setFormIndex(formIndex + 1);
-                },
-                handlePrevious:
-                  formIndex === 0
-                    ? false
-                    : fieldsUpdate => {
-                        formList[formIndex].changes = {
-                          ...formList[formIndex].changes,
-                          ...fieldsUpdate
-                        };
-                        setFormIndex(formIndex - 1);
-                      }
+    return (
+      <React.Fragment>
+        <Modal
+          open={gedsIndex === null}
+          closeOnDimmerClick={false}
+          closeOnEscape={false}
+        >
+          <Modal.Content>
+            <Card.Group itemsPerRow={3}>
+              {gedsInfoList.map((element, index) => {
+                return (
+                  <Card onClick={(e, o) => setGedsIndex(index)}>
+                    <h2>{element.givenName + " " + element.surname}</h2>
+                    <h3>{element.title}</h3>
+                    <h4>{element.email}</h4>
+                    <h4>{element.phoneNumber}</h4>
+                  </Card>
+                );
               })}
-            </Card.Content>
-          </Card>
-        </Grid.Column>
-      </Grid>
+            </Card.Group>
+          </Modal.Content>
+        </Modal>
+        <Grid style={{ maxWidth: "1250px", margin: "0px auto" }}>
+          <Grid.Column width={4}>{this.renderButtons()}</Grid.Column>
+          <Grid.Column width={12}>
+            <Card fluid>
+              <Card.Header>{formList[formIndex].name}</Card.Header>
+              <Card.Content>
+                {React.createElement(formList[formIndex].form, {
+                  handleRegister: handleRegister,
+                  setFormChanges: setFormChanges,
+                  editProfileOptions: editProfileOptions,
+
+                  profileInfo: profileInfo,
+                  handleNext: fieldsUpdate => {
+                    formList[formIndex].changes = {
+                      ...formList[formIndex].changes,
+                      ...fieldsUpdate
+                    };
+                    setFormIndex(formIndex + 1);
+                  },
+                  handlePrevious:
+                    formIndex === 0
+                      ? false
+                      : fieldsUpdate => {
+                          formList[formIndex].changes = {
+                            ...formList[formIndex].changes,
+                            ...fieldsUpdate
+                          };
+                          setFormIndex(formIndex - 1);
+                        }
+                })}
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid>
+      </React.Fragment>
     );
   }
 
