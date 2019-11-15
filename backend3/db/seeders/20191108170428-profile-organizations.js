af835bd403bf64d7cc520c99823b4a14a372a9d7
Mamadou Bah <bmama067@uottawa.ca>
2019-11-15 10:16:03 -0500

    Update 20191108170428-profile-organizations.js

--- backend3/db/seeders/20191108170428-profile-organizations.js
+++ backend3/db/seeders/20191108170428-profile-organizations.js
@@ -10,70 +10,82 @@ module.exports = {
       "profileOrganizations",
       [
         {
-          organization: "Innovation, Science and Economic Development Canada",
+          description_en: "Innovation, Science and Economic Development Canada",
+          description_fr:
+            "Innovation, Sciences et Développement économique Canada",
           tier: "0",
           createdAt: new Date(),
           updatedAt: new Date(),
           profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
         },
         {
-          organization: "DIGITAL TRANSFORMATION SERVICE SECTOR",
+          description_en: "DIGITAL TRANSFORMATION SERVICE SECTOR",
+          description_fr: "SECTEUR DES SERVICES DE TRANSFORMATION NUMERIQUE",
           tier: "1",
           createdAt: new Date(),
           updatedAt: new Date(),
           profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
         },
         {
-          organization: "Chief Information Office",
+          description_en: "Chief Information Office",
+          description_fr: "Bureau de l'information en chef",
           tier: "2",
           createdAt: new Date(),
           updatedAt: new Date(),
           profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
         },
         {
-          organization: "Digital Services Division",
+          description_en: "Digital Services Division",
+          description_fr: "Division des services numériques",
           tier: "3",
           createdAt: new Date(),
           updatedAt: new Date(),
           profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
         },
         {
-          organization: "Business Line Solutions Directorate",
+          description_en: "Business Line Solutions Directorate",
+          description_fr: "Direction des solutions métiers",
           tier: "4",
           createdAt: new Date(),
           updatedAt: new Date(),
           profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
         },
         {
-          organization: "Innovation, Science and Economic Development Canada",
+          description_en: "Innovation, Science and Economic Development Canada",
+          description_fr:
+            "Innovation, Sciences et Développement économique Canada",
           tier: "0",
           createdAt: new Date(),
           updatedAt: new Date(),
           profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
         },
         {
-          organization: "DIGITAL TRANSFORMATION SERVICE SECTOR",
+          description_en: "DIGITAL TRANSFORMATION SERVICE SECTOR",
+          description_fr: "SECTEUR DES SERVICES DE TRANSFORMATION NUMERIQUE",
           tier: "1",
           createdAt: new Date(),
           updatedAt: new Date(),
           profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
         },
         {
-          organization: "Chief Information Office",
+          description_en: "Chief Information Office",
+          description_fr: "Bureau de l'information en chef",
           tier: "2",
           createdAt: new Date(),
           updatedAt: new Date(),
           profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
         },
         {
-          organization: "Digital Services Division",
+          description_en: "Digital Services Division",
+          description_fr: "Division des services numériques",
           tier: "3",
           createdAt: new Date(),
           updatedAt: new Date(),
           profileId: "faba08aa-ffe3-11e9-8d71-362b9e155667"
         },
         {
-          organization: "Director General's Office",
+          description_en: "Director General's Office",
+          description_fr: "Bureau du directeur général",
           tier: "4",
           createdAt: new Date(),
           updatedAt: new Date(),
