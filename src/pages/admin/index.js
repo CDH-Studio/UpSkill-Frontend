39c34c371c888b5d4e6f4be666547d1d5010b789
Mamadou Bah <bmama067@uottawa.ca>
2019-12-03 16:29:35 -0500

    Added dashboard page for admin

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -2,5 +2,12 @@ import AdminSkill from "./Skill";
 import AdminCompetency from "./Competency";
 import AdminDiploma from "./Diploma";
 import AdminSchool from "./School";
+import AdminDasboard from "./School";
 
-export { AdminSkill, AdminCompetency, AdminDiploma, AdminSchool };
+export {
+  AdminSkill,
+  AdminCompetency,
+  AdminDiploma,
+  AdminSchool,
+  AdminDasboard
+};
