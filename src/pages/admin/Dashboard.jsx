464d5d8410277cbb289262dac3a20b0f98cd360d
Mamadou Bah <bmama067@uottawa.ca>
2019-12-04 13:00:15 -0500

    Fixed json file

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,16 +1,56 @@
 import React, { Component } from "react";
-
-import _ from "lodash";
-import { FormattedMessage } from "react-intl";
 import AdminMenu from "../../components/admin/AdminMenu";
+import {
+  Table,
+  Header,
+  Button,
+  Icon,
+  Image,
+  Statistic
+} from "semantic-ui-react";
+import _ from "lodash";
+import axios from "axios";
+import moment from "moment";
+import { FormattedMessage, injectIntl } from "react-intl";
+import config from "../../config";
+const { backendAddress } = config;
 
 class AdminDashboard extends Component {
   goto = (link, state) => this.props.history.push(link, state);
 
   constructor(props) {
     super(props);
-    this.state = { type: "dashboard", loading: false };
+    this.state = { type: "dashboard", loading: true };
   }
+
+  componentDidMount() {
+    // console.log(this.getDisplayType(true));
+
+    document.title = this.getDisplayType(true) + " - Admin | UpSkill";
+    this.setState({ loading: false });
+    // axios.get(backendAddress + "api/admin/" + this.state.type).then(res =>
+    //   this.setState({
+    //     allData: res.data,
+    //     data: _.sortBy(res.data, ["firstName"]),
+    //     loading: false,
+    //     column: "name",
+    //     direction: "ascending"
+    //   })
+    // );
+  }
+
+  getDisplayType = plural => {
+    if (plural)
+      return this.props.intl.formatMessage({
+        id: "admin." + this.state.type + ".plural",
+        defaultMessage: this.state.type
+      });
+
+    return this.props.intl.formatMessage({
+      id: "admin." + this.state.type + ".singular",
+      defaultMessage: this.state.type
+    });
+  };
 
   render() {
     const { type, loading } = this.state;
@@ -24,10 +64,40 @@ class AdminDashboard extends Component {
         loading={loading}
         goto={this.goto}
       >
-        <h1>Welcome to your the Admin dashboard</h1>
+        <Header as="h1">{this.getDisplayType(true)}</Header>
+        <Statistic.Group>
+          <Statistic>
+            <Statistic.Value>22</Statistic.Value>
+            <Statistic.Label>Saves</Statistic.Label>
+          </Statistic>
+
+          <Statistic>
+            <Statistic.Value text>
+              Three
+              <br />
+              Thousand
+            </Statistic.Value>
+            <Statistic.Label>Signups</Statistic.Label>
+          </Statistic>
+
+          <Statistic>
+            <Statistic.Value>
+              <Icon name="plane" />5
+            </Statistic.Value>
+            <Statistic.Label>Flights</Statistic.Label>
+          </Statistic>
+
+          <Statistic>
+            <Statistic.Value>
+              <Image src="/images/avatar/small/joe.jpg" inline circular />
+              42
+            </Statistic.Value>
+            <Statistic.Label>Team Members</Statistic.Label>
+          </Statistic>
+        </Statistic.Group>
       </AdminMenu>
     );
   }
 }
 
-export default AdminDashboard;
+export default injectIntl(AdminDashboard);
