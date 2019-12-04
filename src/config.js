dc29b4c27711a569849938d62364e97297ee39a0
Rizvi <rizvida1@gmail.com>
2019-12-04 13:30:56 -0500

    Hardcoded backend address

--- src/config.js
+++ src/config.js
@@ -3,5 +3,7 @@ require("dotenv").config();
 localStorage.setItem("api", process.env.REACT_APP_API_ADDRESS);
 
 export default {
-  backendAddress: process.env.REACT_APP_API_ADDRESS
+  backendAddress:
+    "http://upskill-backend-upskill.apps.dev.openshift.ised-isde.canada.ca/"
+  // backendAddress: process.env.REACT_APP_API_ADDRESSs
 };
