a9d9ba816fcef27f18da0521d3551aaaf88c8fdf
Rizvi <rizvida1@gmail.com>
2019-12-05 15:54:12 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  // backendAddress:
-  //   "http://upskill-backend-upskill.apps.dev.openshift.ised-isde.canada.ca/"
-  backendAddress: process.env.REACT_APP_API_ADDRESSs
+  backendAddress:
+    "http://upskill-backend-upskill.apps.dev.openshift.ised-isde.canada.ca/"
+  // backendAddress: process.env.REACT_APP_API_ADDRESSs
 };
