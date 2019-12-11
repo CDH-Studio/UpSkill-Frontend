e2e45fc6d71f5b076b97e636ff405fa86001fe4f
Mamadou Bah <bmama067@uottawa.ca>
2019-12-11 17:20:33 -0500

    Commented out code for copy token button

--- src/components/routes/Secured.js
+++ src/components/routes/Secured.js
@@ -79,23 +79,20 @@ class Secured extends Component {
                 <div>
                 {/* Added for copying token ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/}
             {/* <div>
-                  <form>
-                    <textarea
-                      ref={textarea => (this.textArea = textarea)}
-                      value={keycloak.token}
-                    />
-                  </form>
-                  {document.queryCommandSupported("copy") && (
-                    <div>
-                      <button onClick={this.copyToClipboard}>Copy</button>
-                      {this.state.copySuccess}
-                    </div>
-                  )}
+              <form>
+                <textarea
+                  ref={textarea => (this.textArea = textarea)}
+                  value={keycloak.token}
+                />
+              </form>
+              {document.queryCommandSupported("copy") && (
+                <div>
+                  <button onClick={this.copyToClipboard}>Copy</button>
+                  {this.state.copySuccess}
                 </div>
-                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/}
-
+              )}
+            </div> */}
             {/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/}
-
             <Route
               exact
               path="/secured/profile-generation"
