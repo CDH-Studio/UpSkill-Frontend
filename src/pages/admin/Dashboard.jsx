44439f32c33c3b3387681545ed8229e04afd0d65
Sukhu Sekhon <35780281+SukhsimranpreetSekhon@users.noreply.github.com>
2020-02-13 11:01:25 -0500

    [Update] Admin Dashboard

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -16,9 +16,6 @@ import axios from "axios";
 import moment from "moment";
 import { FormattedMessage, injectIntl } from "react-intl";
 import {
-  PieChart,
-  Pie,
-  Sector,
   Cell,
   Legend,
   Tooltip,
@@ -28,7 +25,8 @@ import {
   XAxis,
   YAxis,
   CartesianGrid,
-  ComposedChart
+  ComposedChart,
+  Line
 } from "recharts";
 
 import config from "../../config";
@@ -50,6 +48,14 @@ const developmentGoalColours = [
   "#07748C",
   "#E1DAD2",
   "#8E0000"
+];
+
+const growthRateColours = [
+  "#3548AF",
+  "#4392F1",
+  "#78C0E0",
+  "#6F9CEB",
+  "#141B41"
 ];
 
 class AdminDashboard extends Component {
@@ -86,6 +92,50 @@ class AdminDashboard extends Component {
       id: "admin." + this.state.type + ".singular",
       defaultMessage: this.state.type
     });
+  };
+
+  numberOfOccurrences() {
+    return this.props.intl.formatMessage({
+      id: "admin.dashboard.number.of.occurrences"
+    });
+  }
+
+  numberOfNewUsers() {
+    return this.props.intl.formatMessage({
+      id: "admin.dashboard.number.of.new.users"
+    });
+  }
+
+  CustomizedAxisTick = props => {
+    const { x, y, payload } = props;
+
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text dy={16} textAnchor="middle" fill="#666">
+          {payload.value}
+        </text>
+      </g>
+    );
+  };
+
+  CustomizedYAxisLabel = props => {
+    const { x, y, payload } = props;
+
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text
+          dy={16}
+          offset={0}
+          fontSize={12}
+          text-align="center"
+          textAnchor="end"
+          fill="#666"
+          transform="rotate(-90)"
+        >
+          {this.numberOfNewUsers()}
+        </text>
+      </g>
+    );
   };
 
   render() {
@@ -143,8 +193,55 @@ class AdminDashboard extends Component {
         });
       }
 
+      // Growth Rate By Month:
+      let monthlyGrowthRate = data.growthRateByMonth;
+
+      const months = moment.monthsShort();
+
+      monthlyGrowthRate = monthlyGrowthRate.map((entry, index) => {
+        let addData = [];
+
+        for (let i = 0; i < 12; i++) {
+          addData.push({
+            month: i,
+            monthName: months[i],
+            count: entry.data[i].count
+          });
+        }
+        entry.data = addData;
+        return entry;
+      });
+
+      // Users Added This Month & Growth Rate Percentage:
+      const current_year = moment().year();
+
+      const current_month = moment().month();
+
+      const indexYear = monthlyGrowthRate.findIndex(
+        object => object.year == current_year
+      );
+
+      const current_month_additions = monthlyGrowthRate[indexYear].data.find(
+        object => object.month == current_month
+      );
+
+      let previous_month_additions = {};
+
+      if (current_month == 0) {
+        previous_month_additions = monthlyGrowthRate[indexYear - 1].data.find(
+          object => object.month == 11
+        );
+      } else {
+        previous_month_additions = monthlyGrowthRate[indexYear].data.find(
+          object => object.month == current_month - 1
+        );
+      }
+      const growthRate =
+        ((current_month_additions.count - previous_month_additions.count) /
+          previous_month_additions.count) *
+        100;
+
       const dashboardCount = data.dashboardCount;
-      // const { user, profile, flagged, inactive } = this.state.data;
       const { changeLanguage, keycloak } = this.props;
 
       return (
@@ -198,136 +295,227 @@ class AdminDashboard extends Component {
             </Statistic>
           </Statistic.Group>
 
+          <Statistic.Group color="blue" widths={16}>
+            <Statistic>
+              <Statistic.Value>
+                <Icon name="add user" color="purple" />{" "}
+                {current_month_additions.count}
+              </Statistic.Value>
+              <Statistic.Label>
+                <FormattedMessage id="admin.dashboard.monthly.added" />
+              </Statistic.Label>
+            </Statistic>
+            <Statistic>
+              <Statistic.Value>
+                <Icon name="chart line" color="orange" /> {growthRate}
+                {"%"}
+              </Statistic.Value>
+              <Statistic.Label>
+                <FormattedMessage id="admin.dashboard.growth.rate.percentage" />
+              </Statistic.Label>
+            </Statistic>
+          </Statistic.Group>
+
           <Card fluid>
             <Card.Content>
-              <Card.Header>Commonalities</Card.Header>
+              <Card.Header>
+                <FormattedMessage id="admin.dashboard.commonalities" />
+              </Card.Header>
             </Card.Content>
             <Card.Content>
               <Card.Group>
                 <Card fluid>
                   <Card.Content>
-                    <Card.Header>Common Skills</Card.Header>
+                    <Card.Header>
+                      <FormattedMessage id="admin.dashboard.popular.skills" />
+                    </Card.Header>
                   </Card.Content>
                   <Card.Content>
-                    <BarChart
-                      width={1000}
-                      height={500}
-                      layout="vertical"
-                      data={topFiveSkills}
-                      margin={{
-                        top: 5,
-                        right: 30,
-                        left: 100,
-                        bottom: 5
-                      }}
-                      barCategoryGap="20%"
-                    >
-                      <CartesianGrid strokeDasharray="3 3" />
-                      <XAxis
-                        type="number"
-                        label={{
-                          value: "# of Occurences",
-                          position: "insideBottomRight",
-                          offset: 0,
-                          dy: 2
+                    <ResponsiveContainer aspect={2}>
+                      <BarChart
+                        layout="vertical"
+                        data={topFiveSkills}
+                        margin={{
+                          top: 10,
+                          right: 30,
+                          left: 50,
+                          bottom: 5
                         }}
-                      />
-                      <YAxis type="category" dataKey="name" />
-                      <Tooltip />
-                      <Bar dataKey="count">
-                        {topFiveSkills.map((entry, index) => (
-                          <Cell
-                            key={`cell-${index}`}
-                            fill={skillColours[index]}
-                          />
-                        ))}
-                      </Bar>
-                    </BarChart>
+                        barCategoryGap="20%"
+                      >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis
+                          type="number"
+                          label={{
+                            value: this.numberOfOccurrences(),
+                            position: "insideBottomRight",
+                            offset: 0,
+                            dy: 2,
+                            style: { fontSize: 10 }
+                          }}
+                          tick={<this.CustomizedAxisTick />}
+                        />
+                        <YAxis
+                          type="category"
+                          dataKey="name"
+                          tick={{ interval: 0, style: { fontSize: 12 } }}
+                        />
+                        <Tooltip />
+                        <Bar dataKey="count">
+                          {topFiveSkills.map((entry, index) => (
+                            <Cell
+                              key={`cell-${index}`}
+                              fill={skillColours[index]}
+                            />
+                          ))}
+                        </Bar>
+                      </BarChart>
+                    </ResponsiveContainer>
                   </Card.Content>
                 </Card>
                 <Card fluid>
                   <Card.Content>
-                    <Card.Header>Common Competencies</Card.Header>
+                    <Card.Header>
+                      <FormattedMessage id="admin.dashboard.popular.competencies" />
+                    </Card.Header>
                   </Card.Content>
                   <Card.Content>
-                    <BarChart
-                      width={1000}
-                      height={500}
-                      layout="vertical"
-                      data={topFiveCompetencies}
-                      margin={{
-                        top: 5,
-                        right: 30,
-                        left: 100,
-                        bottom: 5
-                      }}
-                      barCategoryGap="20%"
-                    >
-                      <CartesianGrid strokeDasharray="3 3" />
-                      <XAxis
-                        type="number"
-                        label={{
-                          value: "# of Occurences",
-                          position: "insideBottomRight",
-                          offset: 0,
-                          dy: 2
+                    <ResponsiveContainer aspect={2}>
+                      <BarChart
+                        layout="vertical"
+                        data={topFiveCompetencies}
+                        margin={{
+                          top: 10,
+                          right: 30,
+                          left: 50,
+                          bottom: 5
                         }}
-                      />
-                      <YAxis type="category" dataKey="name" />
-                      <Tooltip />
-                      <Bar dataKey="count">
-                        {topFiveCompetencies.map((entry, index) => (
-                          <Cell
-                            key={`cell-${index}`}
-                            fill={competencyColours[index]}
-                          />
-                        ))}
-                      </Bar>
-                    </BarChart>
+                        barCategoryGap="20%"
+                      >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis
+                          type="number"
+                          label={{
+                            value: this.numberOfOccurrences(),
+                            position: "insideBottomRight",
+                            offset: 0,
+                            dy: 2,
+                            style: { fontSize: 10 }
+                          }}
+                          tick={<this.CustomizedAxisTick />}
+                        />
+                        <YAxis
+                          type="category"
+                          dataKey="name"
+                          tick={{ interval: 0, style: { fontSize: 12 } }}
+                        />
+                        <Tooltip />
+                        <Bar dataKey="count">
+                          {topFiveCompetencies.map((entry, index) => (
+                            <Cell
+                              key={`cell-${index}`}
+                              fill={competencyColours[index]}
+                            />
+                          ))}
+                        </Bar>
+                      </BarChart>
+                    </ResponsiveContainer>
                   </Card.Content>
                 </Card>
                 <Card fluid>
                   <Card.Content>
-                    <Card.Header>Common Development Goals</Card.Header>
+                    <Card.Header>
+                      <FormattedMessage id="admin.dashboard.popular.development.goals" />
+                    </Card.Header>
                   </Card.Content>
                   <Card.Content>
-                    <BarChart
-                      width={1000}
-                      height={500}
-                      layout="vertical"
-                      data={topFiveDevelopmentGoals}
-                      margin={{
-                        top: 5,
-                        right: 30,
-                        left: 100,
-                        bottom: 5
-                      }}
-                      barCategoryGap="20%"
-                    >
-                      <CartesianGrid strokeDasharray="3 3" />
-                      <XAxis
-                        type="number"
-                        label={{
-                          value: "# of Occurences",
-                          position: "insideBottomRight",
-                          offset: 0,
-                          dy: 2
+                    <ResponsiveContainer aspect={2}>
+                      <BarChart
+                        layout="vertical"
+                        data={topFiveDevelopmentGoals}
+                        margin={{
+                          top: 10,
+                          right: 30,
+                          left: 50,
+                          bottom: 5
                         }}
-                      />
-                      <YAxis type="category" dataKey="name" />
-                      <Tooltip />
-                      <Bar dataKey="count">
-                        {topFiveDevelopmentGoals.map((entry, index) => (
-                          <Cell
-                            key={`cell-${index}`}
-                            fill={developmentGoalColours[index]}
-                          />
-                        ))}
-                      </Bar>
-                    </BarChart>
+                        barCategoryGap="20%"
+                      >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis
+                          type="number"
+                          label={{
+                            value: this.numberOfOccurrences(),
+                            position: "insideBottomRight",
+                            offset: 0,
+                            dy: 2,
+                            style: { fontSize: 10 }
+                          }}
+                          tick={<this.CustomizedAxisTick />}
+                        />
+                        <YAxis
+                          type="category"
+                          dataKey="name"
+                          tick={{ interval: 0, style: { fontSize: 12 } }}
+                        />
+                        <Tooltip />
+                        <Bar dataKey="count">
+                          {topFiveDevelopmentGoals.map((entry, index) => (
+                            <Cell
+                              key={`cell-${index}`}
+                              fill={developmentGoalColours[index]}
+                            />
+                          ))}
+                        </Bar>
+                      </BarChart>
+                    </ResponsiveContainer>
                   </Card.Content>
                 </Card>
               </Card.Group>
+            </Card.Content>
+          </Card>
+
+          <Card fluid>
+            <Card.Content>
+              <Card.Header>
+                <FormattedMessage id="admin.dashboard.growth.rate.by.month" />
+              </Card.Header>
+            </Card.Content>
+            <Card.Content>
+              <ResponsiveContainer aspect={2}>
+                <ComposedChart
+                  data={monthlyGrowthRate}
+                  margin={{ top: 20, right: 30, left: 0, bottom: 10 }}
+                >
+                  <CartesianGrid />
+                  <XAxis
+                    dataKey="monthName"
+                    type="category"
+                    allowDuplicatedCategory={false}
+                    data={monthlyGrowthRate}
+                    tick={<this.CustomizedAxisTick />}
+                  />
+                  <YAxis
+                    type="number"
+                    dataKey="count"
+                    label={this.CustomizedYAxisLabel}
+                  />
+                  <Tooltip />
+                  <Legend />
+                  {monthlyGrowthRate.map((period, index) => (
+                    <Line
+                      dataKey="count"
+                      data={period.data}
+                      name={period.year}
+                      key={period.year}
+                      type="monotone"
+                      stroke={growthRateColours[index]}
+                      strokeWidth={3}
+                      activeDot={{ r: 8 }}
+                    />
+                  ))}
+                </ComposedChart>
+              </ResponsiveContainer>
             </Card.Content>
           </Card>
         </AdminMenu>
